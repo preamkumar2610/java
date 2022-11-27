03eb7c18264b77b6d71b118f82c8dc03dd50176a
preamkumar2610 <preamkumar2610@gmail.com>
2022-11-27 11:30:49 +0530

    the code will be added

---
+++ src/org/sample/Task1.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class Task1 {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+	}
+
+}
